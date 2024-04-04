109fba1366f4e53acee9b7abc3df0ea8d5d0ba02
junnie082 <junnie082@gmail.com>
2024-04-04 20:16:57 +0900

    feat: developed the function to compare the article and the input

--- ias/function.py
+++ ias/function.py
@@ -1,7 +1,44 @@
-def cmpArticleInput(input, article):
-    input = input.replace(' ','')
-    article = article.replace(' ','')
+def preprocess_text(text):
+    # Replace curly apostrophes with straight apostrophes
+    return text.replace('’', "'")
+
+
+def cmpInputArticle(input, article):
+    input = preprocess_text(input.replace(' ', ''))
+    article = preprocess_text(article.replace(' ', ''))
     if input == article:
         return True
     return False
+
+def chkErrors(input, article):
+    inputList = list(input.split())
+    articleList = list(article.split())
+    returnList = list()
+
+    for index in range(len(articleList)):
+        word = ""
+
+        if index >= len(inputList):
+            for i in range(len(articleList[index])):
+                word += '@'
+            print("word: " + word)
+
+        else:
+            for i in range(len(articleList[index])):
+                if i >= len(inputList[index]):
+                    word += '@'
+                elif inputList[index][i] == "'" and articleList[index][i] == "’":
+                    word += inputList[index][i]
+                elif inputList[index][i] != articleList[index][i]:
+                    word += '@'
+                else:
+                    word += inputList[index][i]
+
+        returnList.append(word)
+        print("returnList: " + str(returnList))
+
+    return returnList
+
+
+
 
